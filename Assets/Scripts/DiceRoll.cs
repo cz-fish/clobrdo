101a797c5ca4a202d2bfe0c6cdec6b394d8e480f
Filip Simek <filip.simek@gmail.com>
2021-01-10 13:40:56 +0000

    Make sure dice always stops after some time

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -8,6 +8,9 @@ public class DiceRoll : MonoBehaviour
 
     private Rigidbody m_rb;
     private bool m_hiding = false;
+    private float m_rollingTime = 0;
+
+    private const float MaxRollingTime = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +21,21 @@ public class DiceRoll : MonoBehaviour
     // Update is called once per frame, when the dice is active and rolling
     void Update()
     {
-        if (!m_hiding && m_rb.velocity.magnitude < 0.0001f && GetComponent<Transform>().position.y < 1f) {
-            // the dice has stopped
-            var dice = GetComponent<Die_d6>();
+        if (!m_hiding) {
+            // the dice is rolling
+            m_rollingTime += Time.deltaTime;
 
-            //if (dice.value > 0) {
+            if ((m_rb.velocity.magnitude < 0.0001f && GetComponent<Transform>().position.y < 1f)  // the dice has stopped
+                || (m_rollingTime > MaxRollingTime)  // or the dice has been rolling for too long
+            ) {
+                // take the dice value
+                var dice = GetComponent<Die_d6>();
                 gameState.OnDiceRoll(dice.value);
-            //}
 
-            m_hiding = true;
-            // deactivate and hide the dice after 1 second
-            StartCoroutine(HideDice());
+                m_hiding = true;
+                // deactivate and hide the dice after a bit of time
+                StartCoroutine(HideDice());
+            }
         }
     }
 
@@ -36,6 +43,7 @@ public class DiceRoll : MonoBehaviour
         yield return new WaitForSeconds(0.4f);
         Debug.Log("Hiding the dice");
         m_hiding = false;
+        m_rollingTime = 0f;
         gameObject.SetActive(false);
     }
 }
