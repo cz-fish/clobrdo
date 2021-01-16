e66e9f069fa55f3299f1918ccdc26ece6d06a066
Filip Simek <filip.simek@gmail.com>
2021-01-16 22:08:35 +0000

    Add mouse manager, improve colliders

---
+++ Assets/Scripts/MouseSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelection : MonoBehaviour
+{
+    public GameObject hoverObject;
+
+    void OnMouseDown()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo)) {
+            hoverObject = hitInfo.transform.root.gameObject;
+        } else {
+            hoverObject = null;
+        }
+    }
+}
